27e4e52d8ef341a3ef1b4be942aa6d23a9c7a126
[MarcoAbreu2002] <[marcoabreu2002@gmail.com]>
2023-05-20 13:46:04 +0100

    random objects, rotating, differente colours

--- src/js/three.js
+++ src/js/three.js
@@ -1,38 +1,107 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize( 800, 600 );
-
-document.body.appendChild( renderer.domElement );
-
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 
-    45, 
-    window.innerWidth / window.innerHeight, 
-    0.1, 
-    1000 
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 
-const orbit = new OrbitControls( camera, renderer.domElement );
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#gl-canvas'),
+});
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(800, 600);
+camera.position.setZ(20);
 
-camera.position.set(-10, 30, 30);
-orbit.update();
+const objectsCount = Math.floor(Math.random() * 26) + 5;
+console.log(objectsCount);
 
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const box = new THREE.Mesh( boxGeometry, boxMaterial );
-scene.add( box );
+const meshes = [];
 
-function animate(time) {
-    box.rotation.x = time / 1000;
-    box.rotation.y = time / 1000;
-    renderer.render( scene, camera );
+for (let i = 0; i < objectsCount; i++) {
+  console.log('Loop iteration:', i);
+  const randomX = Math.random() * 20 - 10;
+  const randomY = Math.random() * 2 - 1;
+  const randomZ = Math.random() * 20 - 10;
+
+  let geometry, material;
+  const randomShape = Math.random() < 0.5 ? 'cube' : 'pyramid';
+
+  if (randomShape === 'cube') {
+    const cubeSize = Math.random() * 0.4 + 0.1;
+    geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    material = new THREE.MeshStandardMaterial({
+      color: getRandomColor(),
+      
+    });
+    console.log('cube');
+  } else if (randomShape === 'pyramid') {
+    const pyramidSize = Math.random() * 0.4 + 0.1;
+    geometry = new THREE.TetrahedronBufferGeometry(pyramidSize, 0); // radius and detail
+    material = new THREE.MeshStandardMaterial({
+      color: getRandomColor(),
+    });
+    console.log('pyramid');
+  }
+
+  const mesh = new THREE.Mesh(geometry, material);
+  console.log('MESH: ' + mesh);
+  mesh.position.set(randomX, randomY, randomZ);
+  scene.add(mesh);
+  meshes.push(mesh);
+  console.log('MESH: ' + mesh);
 }
 
-renderer.setAnimationLoop( animate );
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(8, 8, 8);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function getRandomColor() {
+  return Math.random() * 0xffffff;
+}
+
+function animateRandomRotation() {
+  for (const mesh of meshes) {
+    const randomSpeedX = (Math.random() - 0.5) * 0.1;
+    const randomSpeedY = (Math.random() - 0.5) * 0.1;
+    const randomSpeedZ = (Math.random() - 0.5) * 0.1;
+
+    function update() {
+      mesh.rotation.x += randomSpeedX;
+      mesh.rotation.y += randomSpeedY;
+      mesh.rotation.z += randomSpeedZ;
+    }
+
+    animateFunctions.push(update);
+  }
+}
+
+const animateFunctions = [];
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  for (const animateFunction of animateFunctions) {
+    animateFunction();
+  }
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animateRandomRotation();
+animate();
